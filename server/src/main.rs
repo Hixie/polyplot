8ce82d6be076cf572a7290289b31c56c32538f43
Ian Hickson <ian@hixie.ch>
2022-12-23 02:27:46 -0800

    try using streams of results instead (still does not work)

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,8 +8,9 @@ use std::sync::Mutex;
 use futures_util::sink::SinkExt; // for send
 use futures_util::stream::StreamExt; // for split
 use futures_util::stream::TryStreamExt; // for try_for_each
+use futures_util::future::TryFutureExt; // for and_then
 
-type PeerMap = Arc<Mutex<HashMap<std::net::SocketAddr, futures_channel::mpsc::UnboundedSender<String>>>>;
+type PeerMap = Arc<Mutex<HashMap<std::net::SocketAddr, futures_channel::mpsc::UnboundedSender<Result<String, ()>>>>>;
 
 async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsStream<tokio::net::TcpStream>, addr: std::net::SocketAddr) {
   println!("{}: Connecting...", addr);
@@ -19,19 +20,19 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsS
       .expect("Error during the websocket handshake occurred");
   println!("{}: Connection established.", addr);
 
-  let (internal_tx, internal_rx) = futures_channel::mpsc::unbounded::<String>();
+  let (internal_tx, internal_rx) = futures_channel::mpsc::unbounded::<Result<String, ()>>();
   peer_map.lock().unwrap().insert(addr, internal_tx);
 
   let (mut websocket_tx, websocket_rx) = ws_stream.split();
 
-  let incoming_websocket_messages = websocket_rx.try_for_each(|message| {
-    match message {
+  let incoming_websocket_messages = websocket_rx.map(|message| {
+    match message.unwrap() {
       tungstenite::protocol::Message::Text(payload) => {
         println!("{}: \"{}\"", addr, payload);
         let peers = peer_map.lock().unwrap();
         let peer_txs = peers.iter().filter(|(peer_addr, _peer_tx)| peer_addr != &&addr).map(|(_peer_addr, peer_tx)| peer_tx);
         for peer_tx in peer_txs {
-          peer_tx.unbounded_send(payload.clone()).unwrap();
+          peer_tx.unbounded_send(Ok(payload.clone())).unwrap();
         }
       },
       tungstenite::protocol::Message::Close(payload) => {
@@ -45,15 +46,12 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsS
     futures_util::future::ok(())
   });
 
-  let incoming_peer_messages = internal_rx.for_each(|message| {
-    // TODO: we drop the following future ("futures_util::sink::Send") on the floor, which means it never sends
-    // but how should that be solved??
-    websocket_tx.send(tungstenite::protocol::Message::Text(message.clone()));
-    futures_util::future::ready(())
+  let incoming_peer_messages = internal_rx.map(|message| {
+    websocket_tx.send(tungstenite::protocol::Message::Text(message.unwrap().clone()))
   });
 
   futures_util::pin_mut!(incoming_websocket_messages, incoming_peer_messages);
-  futures_util::future::select(incoming_websocket_messages, incoming_peer_messages).await;
+  futures_util::stream::select(incoming_websocket_messages, incoming_peer_messages).await;
 
   println!("{}: Disconnected.", &addr);
   peer_map.lock().unwrap().remove(&addr);
