5cae40764495a1f6457f68f2b3bb5777f06b1893
Ian Hickson <ian@hixie.ch>
2022-12-23 16:28:34 -0800

    More appropriate error handling.

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,29 +9,38 @@ use futures_util::sink::SinkExt; // for send
 use futures_util::stream::StreamExt; // for split
 use futures_util::stream::TryStreamExt; // for try_for_each
 
-type PeerMap = Arc<Mutex<HashMap<std::net::SocketAddr, futures_channel::mpsc::UnboundedSender<Result<String, ()>>>>>;
+type PeerMap = Arc<Mutex<HashMap<std::net::SocketAddr, futures_channel::mpsc::UnboundedSender<String>>>>;
 
-async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsStream<tokio::net::TcpStream>, addr: std::net::SocketAddr) {
+async fn handle_connection(peer_map: PeerMap, addr: std::net::SocketAddr, raw_stream: tokio::net::TcpStream, acceptor: tokio_native_tls::TlsAcceptor) {
   println!("{}: Connecting...", addr);
 
-  let ws_stream = tokio_tungstenite::accept_async(raw_stream)
-      .await
-      .expect("Error during the websocket handshake occurred");
-  println!("{}: Connection established.", addr);
+  let tls_stream: Result<tokio_native_tls::TlsStream<tokio::net::TcpStream>, native_tls::Error> = acceptor.accept(raw_stream).await;
+  if let Err(error) = tls_stream {
+    println!("{}: TLS handshake failed: {}", addr, error);
+    return;
+  }
+  let tls_stream = tls_stream.unwrap();
 
-  let (internal_tx, mut internal_rx) = futures_channel::mpsc::unbounded::<Result<String, ()>>();
+  let ws_stream: Result<tokio_tungstenite::WebSocketStream<tokio_native_tls::TlsStream<tokio::net::TcpStream>>, tungstenite::Error> = tokio_tungstenite::accept_async(tls_stream).await;
+  if let Err(error) = ws_stream {
+    println!("{}: WebSocket handshake failed: {}", addr, error);
+    return;
+  }
+  let (mut websocket_tx, websocket_rx) = ws_stream.unwrap().split();
+
+  let (internal_tx, mut internal_rx) = futures_channel::mpsc::unbounded::<String>();
   peer_map.lock().unwrap().insert(addr, internal_tx);
-
-  let (mut websocket_tx, websocket_rx) = ws_stream.split();
+  println!("{}: Connection established.", addr);
 
   let incoming_websocket_messages = websocket_rx.try_for_each(|message| {
     match message {
       tungstenite::protocol::Message::Text(payload) => {
         println!("{}: \"{}\"", addr, payload);
+        // Forward the message to every other peer.
         let peers = peer_map.lock().unwrap();
         let peer_txs = peers.iter().filter(|(peer_addr, _peer_tx)| peer_addr != &&addr).map(|(_peer_addr, peer_tx)| peer_tx);
         for peer_tx in peer_txs {
-          peer_tx.unbounded_send(Ok(payload.clone())).unwrap();
+          peer_tx.unbounded_send(payload.clone()).unwrap_or_default();
         }
       },
       tungstenite::protocol::Message::Close(payload) => {
@@ -47,13 +56,13 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsS
 
   let incoming_peer_messages = async move {
     while let Some(message) = internal_rx.next().await {
-      websocket_tx.send(tungstenite::protocol::Message::Text(message.unwrap().clone())).await.unwrap();
+      websocket_tx.send(tungstenite::protocol::Message::Text(message.clone())).await.unwrap_or_default();
     };
     Ok(())
   };
 
   futures_util::pin_mut!(incoming_websocket_messages, incoming_peer_messages);
-  futures_util::future::try_join(incoming_websocket_messages, incoming_peer_messages).await.unwrap();
+  futures_util::future::try_join(incoming_websocket_messages, incoming_peer_messages).await.unwrap_or_default();
 
   println!("{}: Disconnected.", &addr);
   peer_map.lock().unwrap().remove(&addr);
@@ -63,6 +72,7 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: tokio_native_tls::TlsS
 struct Configuration {
   pfx_filename: String,
   server_address: String,
+  server_port: u16,
   update_interval: core::time::Duration,
 }
 
@@ -72,7 +82,8 @@ async fn read_configuration() -> Configuration {
   Configuration{
     pfx_filename: String::from(lines[0]),
     server_address: String::from(lines[1]),
-    update_interval: core::time::Duration::from_secs(lines[2].parse().unwrap()),
+    server_port: lines[2].parse().expect("Port (line 3) was not an integer in the range 0..65535."),
+    update_interval: core::time::Duration::from_secs(lines[3].parse().expect("Update interval (line 4) was not an integer (number of seconds).")),
   }
 }
 
@@ -86,13 +97,11 @@ async fn prepare_acceptor(configuration: &Configuration) -> tokio_native_tls::Tl
   tokio_native_tls::TlsAcceptor::from(native_tls::TlsAcceptor::builder(pkcs12).build().unwrap())
 }
 
-// TODO: cleaner error handling
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
   let configuration = Arc::new(read_configuration().await);
   let state = PeerMap::new(Mutex::new(HashMap::new()));
-  let listener = tokio::net::TcpListener::bind(&configuration.server_address).await.unwrap();
+  let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", configuration.server_port)).await.unwrap();
 
   let acceptor: Arc<Mutex<tokio_native_tls::TlsAcceptor>> = Arc::new(Mutex::new(prepare_acceptor(&configuration).await));
   tokio::spawn({
@@ -110,19 +119,23 @@ async fn main() -> std::io::Result<()> {
   });
 
   tokio::spawn({
-    println!("Listening on: {}", configuration.server_address);
+    println!("Listening on: {}:{}", configuration.server_address, configuration.server_port);
     let acceptor: Arc<Mutex<tokio_native_tls::TlsAcceptor>> = acceptor.clone();
     async move {
       loop {
-        let (stream, addr) = listener.accept().await.unwrap();
-        let current_acceptor = acceptor.clone().lock().unwrap().clone();
-        tokio::spawn(handle_connection(state.clone(), current_acceptor.accept(stream).await.unwrap(), addr));
+        let socket = listener.accept().await;
+        if let Err(error) = socket {
+          println!("Aborted connection: {}", error);
+          continue;
+        }
+        let (stream, addr) = socket.unwrap();
+        tokio::spawn(handle_connection(state.clone(), addr, stream, acceptor.clone().lock().unwrap().clone()));
       }
     }
   });
 
   tokio::signal::ctrl_c().await.unwrap();
-  println!("Aborting...");
+  println!("\nAborting...");
 
   Ok(())
 }
